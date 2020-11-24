5bf96f5cfb4fb14136993577ca950d81b089f571
Rachel2308 <rachelsherlock@yahoo.co.uk>
2020-11-24 13:03:27 +0000

    Fix bug with commas rendering at the top of the table

--- main.js
+++ main.js
@@ -58,6 +58,6 @@ function writeToDocument(url) {
             
         });    
         
-        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`;
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`.replace(/,/g, "");
     });
 }
